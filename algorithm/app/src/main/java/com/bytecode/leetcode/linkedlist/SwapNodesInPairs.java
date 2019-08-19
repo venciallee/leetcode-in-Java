7332103c09ca5f030428f258b28b5824dbd62c40
李文超 <lwc@meitu.com>
2019-08-19 23:19:50 +0800

    更新内容

--- algorithm/app/src/main/java/com/bytecode/leetcode/linkedlist/SwapNodesInPairs.java
+++ algorithm/app/src/main/java/com/bytecode/leetcode/linkedlist/SwapNodesInPairs.java
@@ -18,4 +18,43 @@ package com.bytecode.leetcode.linkedlist;
  * Created by vencial on 2019-08-19.
  */
 public class SwapNodesInPairs {
+
+    public static void main(String args[]) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+
+        ListNode node = new SwapNodesInPairs().swapPairs(head);
+
+        ListNode target = new ListNode(2);
+        target.next = new ListNode(1);
+        target.next.next = new ListNode(4);
+        target.next.next.next = new ListNode(3);
+
+        while (node != null) {
+            if (node.val != target.val) {
+                throw new RuntimeException("Error!");
+            }
+            target = target.next;
+            node = node.next;
+        }
+        System.out.println("Pass!");
+    }
+
+    /**
+     * 1. 递归与交换, 由于该题目的规律性, 可使用递归解决
+     * <p>
+     * 1. recursive and swap node.
+     *
+     * @param head head
+     * @return ListNode
+     */
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode next = head.next;
+        head.next = swapPairs(head.next.next);
+        next.next = head;
+        return next;
+    }
 }
